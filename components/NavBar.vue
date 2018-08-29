a61eb0af17f95ac2d603a94fd80dbb4e4949a494
Julia Ebert <julia@juliaebert.com>
2018-08-29 01:13:56 -0700

    Looking prettier but seriously lacking content. Also probably looks like trash on mobile. Haven't even bothered to check

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,9 +2,9 @@
     <nav class="navbar is-fixed-top square-nav">
       <div class="container">
         <div class="navbar-brand">
-          <a class="navbar-item">
+          <!-- <a class="navbar-item">
             <img src="/imgs/icons/misc.svg" alt="Logo">
-          </a>
+          </a> -->
           <span class="navbar-burger burger" data-target="navbarMenuHeroA">
             <span></span>
             <span></span>
@@ -13,12 +13,25 @@
         </div>
         <div id="navbarMenuHeroA" class="navbar-menu">
           <scrollactive :offset="0" class="navbar-end">
-            <a href="#header" class="navbar-item scrollactive-item">
-              <img src="/imgs/icons/misc.svg">
+            <a href="#who-i-am" class="navbar-item scrollactive-item">
+              <img src="/imgs/icons/who-i-am.svg">
+            </a>
+            <a href="#cv" class="navbar-item scrollactive-item">
+              <img src="/imgs/icons/cv.svg">
             </a>
             <a href="#research" class="navbar-item scrollactive-item">
               <img src="/imgs/icons/research.svg">
             </a>
+            <a href="#publications" class="navbar-item scrollactive-item">
+              <img src="/imgs/icons/publications.svg">
+            </a>
+            <a href="#projects" class="navbar-item scrollactive-item">
+              <img src="/imgs/icons/projects.svg">
+            </a>
+            <a href="#media" class="navbar-item scrollactive-item">
+              <img src="/imgs/icons/media.svg">
+            </a>
+
             </scrollactive>
         </div>
       </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,59 @@
 <template>
 <div>
-  <div class="section scroll-section is-medium has-texture-circuit-board" id="research">
-    <div class="container">
-        <div class="section is-light is-small primary">
-          <img class="icon-block" src="/imgs/icons/research.svg">
-              <h1 class="title is-1">Research</h1>
-              <h3 class="subtitle is-4">Beep boop something robots</h3>
 
-        <div class=flexy>
-            <div class="grid-img">
-              <div class="image is-square">
-                <img src="/imgs/self-csgf.jpg">
-              </div>
-            </div>
-          <div class="grid-content">
-            <h1 class="title is-2">Who I Am</h1>
-            <p>
-              I am a PhD candidate in computer science at Harvard University. I am a member of the Self-organizing Systems Research group, where I study bio-inspired collective robotics with Prof. Radhika Nagpal. I am also a Department of Energy Computational Science Graduate Fellow (DOE CSGF)
-            </p>
-          </div>
+<primary-section title="Who I Am" subtitle="Beep boop robots" texture="graph-paper">
+  <div class=flexy>
+      <div class="grid-img">
+        <div class="image is-square">
+          <img src="/imgs/self-csgf.jpg">
         </div>
       </div>
+    <div class="grid-content">
+      <p>
+        I am a PhD candidate in computer science at Harvard University. I am a member of the Self-organizing Systems Research group, where I study bio-inspired collective robotics with Prof. Radhika Nagpal. I am also a Department of Energy Computational Science Graduate Fellow (DOE CSGF)
+      </p>
     </div>
   </div>
+</primary-section>
 
-<div class="children">
-  <nuxt-child />
-</div>
+<primary-section title="CV" subtitle="Curriculum Vitae"
+  texture="brick-wall">
+  I did things.
+</primary-section>
+
+<primary-section title="Research" subtitle="Robots and Satellites and Humans"
+  texture="circuit-board">
+  I did things.
+</primary-section>
+
+<primary-section title="Publications"
+  texture="bank-note">
+  I wrote things.
+</primary-section>
+
+<primary-section title="Projects" subtitle="I make a lot of mostly useless things."
+  texture="hexagons">
+Hello, world
+</primary-section>
+
+<primary-section title="Media" subtitle="It's an ego boost"
+  texture="diagonal-lines">
+  I did things.
+</primary-section>
+
+
 
 </div>
 </template>
 
 <script>
 import VueMarkdown from "vue-markdown";
+import PrimarySection from "~/components/PrimarySection.vue";
+
 export default {
   components: {
-    VueMarkdown
+    VueMarkdown,
+    PrimarySection
   },
   layout: "alt"
 };
