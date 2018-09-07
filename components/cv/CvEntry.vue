66e03ab739a3e23cdbc06760edac892ac096224e
Julia Ebert <julia@juliaebert.com>
2018-09-07 19:31:49 -0400

    Making CV responsive

--- components/cv/CvEntry.vue
+++ components/cv/CvEntry.vue
@@ -1,29 +1,30 @@
 <template>
 <div class="cv-entry">
   <div class='cv-bar'></div>
-    <div class="cv-sidebar">
-        <b v-if="location" v-html="location"></b>
-        <br v-if="dates || subtitle">
-        <span v-if="dates">{{ dates.replace('--', '–') }}</span>
-    </div>
-    <div class="cv-main">
-        <b>{{ title }}</b>
-        <br v-if="dates || subtitle">
-        <span v-if="subtitle" v-html="subtitle"></span>
-    </div>
+  <div class="cv-sidebar entry-primary">
+      <b v-if="location" v-html="location" class="location"></b>
+      <br v-if="dates || subtitle" class="location">
+      <span v-if="dates">{{ dates.replace('--', '–') }}</span>
+  </div>
+  <div class="cv-main entry-primary">
+      <b>{{ title }}</b>
+      <br v-if="dates || subtitle">
+      <span v-if="subtitle" v-html="subtitle"></span>
+  </div>
 
-    <div v-for="(task, id) in tasks" :key="`task-${id}`" class="cv-sidebar"
-      :style="{gridRow: id+2}">
-        {{ task.date.replace('--', '–') }}
-    </div>
+  <div v-for="(task, id) in tasks" :key="`task-${id}`"
+    :class="['cv-sidebar', 'grid-row-'+(id+2)]"
+    >
+      {{ task.date.replace('--', '–') }}
+  </div>
 
-    <div v-for="(task, id) in tasks" :key="`task-${id}`" class="cv-main"
-      :style="{gridRow: id+2}">
-        <span class="cv-task">
-          <i class="mdi mdi-chevron-right"></i><span v-html="task.task"></span>
-        </span>
-    </div>
-
+  <div v-for="(task, id) in tasks" :key="`task-${id}`"
+    :class="['cv-main', 'grid-row-'+(id+2)]"
+    >
+      <span class="cv-task">
+        <i class="mdi mdi-chevron-right"></i><span v-html="task.task"></span>
+      </span>
+  </div>
 </div>
 </template>
 
