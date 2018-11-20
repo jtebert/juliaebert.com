012699386c52f09f8bed36253ce24cfd382daa2c
Julia Ebert <julia@juliaebert.com>
2018-11-19 20:31:24 -0500

    Add pretty button links on the home page

---
+++ components/ImageBlockLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <nuxt-link class="img-button" :to="'/'+slug">
+        <img :src="'/imgs/icons/'+slug+'.svg'">
+        <h2 class="title">{{ title }}</h2>
+    </nuxt-link>
+</template>
+
+<script>
+import slugify from "slugify";
+
+export default {
+  props: ["title"],
+  computed: {
+    slug: function() {
+      return slugify(this.title, { lower: true });
+    }
+  }
+};
+</script>
