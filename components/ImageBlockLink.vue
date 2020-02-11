8a7bd1aacf37cd458a3344acf2713a00e9c85760
Julia Ebert <julia@juliaebert.com>
2020-02-10 19:10:17 -0500

    Update CV, add Docs, clean up templates/components

--- components/ImageBlockLink.vue
+++ components/ImageBlockLink.vue
@@ -1,6 +1,10 @@
 <template>
-  <nuxt-link class="img-button" :to="'/'+slug">
-    <img :src="'/imgs/icons/'+slug+'.svg'">
+  <a v-if="link" class="img-button" :href=link>
+    <img :src="icon_src">
+    <h2 class="title">{{ title }}</h2>
+  </a>
+  <nuxt-link class="img-button" :to="'/'+slug" v-else>
+    <img :src="icon_src">
     <h2 class="title">{{ title }}</h2>
   </nuxt-link>
 </template>
@@ -9,10 +13,17 @@
 import slugify from "slugify";
 
 export default {
-  props: ["title"],
+  props: ["title", "link", "icon"],
   computed: {
     slug: function() {
       return slugify(this.title, { lower: true });
+    },
+    icon_src: function() {
+      if (this.icon) {
+        return "/imgs/icons/" + this.icon + ".svg";
+      } else {
+        return "/imgs/icons/" + this.slug + ".svg";
+      }
     }
   }
 };
--- layouts/error.vue
+++ layouts/error.vue
@@ -4,9 +4,12 @@
     <h1 v-else class="error-title">Error</h1>
     <h3 class="subtitle is-3">
       <nuxt-link to="/">
-        <i class="mdi mdi-arrow-left"></i>Go Home
+        <i class="mdi mdi-arrow-left"></i>&nbsp;Go Home
       </nuxt-link>
     </h3>
+    <p class="has-text-white">
+      Alternatively, stick around this page and have some fun.<br>(Click or tap on the background and see what happens.)
+    </p>
   </div>
 </template>
 
