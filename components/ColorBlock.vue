1cdbb4dc9044347880794f993b7960dad074a957
Julia Ebert <julia@juliaebert.com>
2020-11-05 18:12:33 -0500

    Update home page and CV

---
+++ components/ColorBlock.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="color-block-wrapper">
+        <div :class="['color-block', 'is-'+color]"></div>
+        <div :class="['color-block', 'is-'+color+'-light']" v-if="showLight"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: { type: String },
+    showLight: { type: Boolean, default: true },
+  },
+};
+</script>
