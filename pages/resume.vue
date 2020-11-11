49b521c1a1015b1dabbb639cf247d2223120f3de
Julia Ebert <julia@juliaebert.com>
2020-11-11 18:28:24 -0500

    Add resume, mostly. Cleaned up cv-entry bug with task descriptions

---
+++ pages/resume.vue
@@ -0,0 +1,33 @@
+<template>
+  <primary-section :title="title" subtitle="Please hire me" texture="brick-wall" icon='cv' class="is-printable">
+    <div slot="links">
+        <icon-button-link to="/pdfs/ebert-julia.resume.pdf" icon="file-pdf">View as PDF</icon-button-link>
+    </div>
+    <div class="block-content content">
+      <resume-content></resume-content>
+    </div>
+  </primary-section>
+</template>
+
+<script>
+import PrimarySection from "~/components/PrimarySection.vue";
+import IconButtonLink from "~/components/IconButtonLink.vue";
+import resumeContent from "~/pages/resumeContent.vue";
+
+export default {
+  components: {
+    PrimarySection,
+    IconButtonLink,
+    resumeContent
+  },
+  data() {
+    return { title: "Resume" };
+  },
+  head() {
+    return {
+      title: this.title + " - " + process.env.siteTitle,
+      meta: [{ hid: "resume" }]
+    };
+  }
+};
+</script>
